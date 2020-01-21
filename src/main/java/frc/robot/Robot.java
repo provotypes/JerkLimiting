65b62cade2b7e08b8f4f2f8f48d63f1fa3253ad8
cabobalot <casey8cockrell@gmail.com>
2020-01-20 17:40:28 -0700

    made it limit acceleration (not yet jerk)

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,46 +2,72 @@ package frc.robot;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
-    
-    CANSparkMax front_left = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
-		CANSparkMax rear_left = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
-		CANSparkMax front_right = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
-		CANSparkMax rear_right = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
 
-   SafeDrivetrain drivetrain = new SafeDrivetrain(
-                                     new SpeedControllerGroup(front_left, rear_left),
-						                       	 new SpeedControllerGroup(front_right, rear_right));
+	Joystick j = new Joystick(0);
+
+	CANSparkMax frontLeft = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
+	CANSparkMax rearLeft = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
+	CANSparkMax frontRight = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
+	CANSparkMax rearRight = new CANSparkMax(4, CANSparkMaxLowLevel.MotorType.kBrushless);
+
+	
+
+	SafeDrivetrain drivetrain = new SafeDrivetrain(
+								new SpeedControllerGroup(frontLeft, rearLeft),
+											new SpeedControllerGroup(frontRight, rearRight));
 
 
-  @Override
-  public void robotInit() {
-  }
+	@Override
+	public void robotInit() {
 
-  @Override
-  public void robotPeriodic() {
-    SmartDashboard.putNumber("Left", front_left.get());
-    SmartDashboard.putNumber("Right", front_right.get());
-    SmartDashboard.getNumber("Max Jerk", 0);
-    SmartDashboard.getNumber("Time Window", 0);
+		frontLeft.setIdleMode(IdleMode.kBrake);
+		rearLeft.setIdleMode(IdleMode.kBrake);
+		frontRight.setIdleMode(IdleMode.kBrake);
+		rearRight.setIdleMode(IdleMode.kBrake);
 
-  }
+		frontLeft.setOpenLoopRampRate(0);
+		rearLeft.setOpenLoopRampRate(0);
+		frontRight.setOpenLoopRampRate(0);
+		rearRight.setOpenLoopRampRate(0);
 
-  @Override
-  public void autonomousInit() {
-   
-  }
+		SmartDashboard.putNumber("Max Jerk", 10);
+		SmartDashboard.putNumber("Time Window", 0);
+		SmartDashboard.putNumber("Max Accel", 0.5);
 
-  @Override
-  public void teleopPeriodic() {
-  }
+	}
 
-  @Override
-  public void testPeriodic() {
-  }
+	@Override
+	public void robotPeriodic() {
+		SmartDashboard.putNumber("Left", -frontLeft.get());
+		SmartDashboard.putNumber("Right", frontRight.get());
+
+		// SmartDashboard.putNumber("stick", j.getRawAxis(1));
+
+		drivetrain.setMaxJerk(SmartDashboard.getNumber("Max Jerk", 0));
+		drivetrain.setTimeWindow(SmartDashboard.getNumber("Time Window", 0));
+		drivetrain.setMaxAcceleration(SmartDashboard.getNumber("Max Accel", 10));
+
+	}
+
+	@Override
+	public void autonomousInit() {
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		drivetrain.arcadeDrive(j.getRawAxis(1), 0);
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
 }
--- src/main/java/frc/robot/SafeDrivetrain.java
+++ src/main/java/frc/robot/SafeDrivetrain.java
@@ -7,48 +7,141 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SafeDrivetrain extends DifferentialDrive {
-private double maxJerk;
-private Timer time;
-private double lastPower;
-private double timeWindow = 1;
-private double maxAcceleration;
+	private double maxJerk = 10;
+	private Timer time = new Timer();
+	private double lastPower = 0;
+	private double lastLastPower = 0; // joystick values are in finite timestamps...
+	private double lastTime = 0;
+	private double lastLastTime = 0;
+	private double timeWindow = 1;
+	private double maxAcceleration = 0.5;
+	private double thisAccel;
+	private double dt = 0;
 
-    public SafeDrivetrain(SpeedController left, SpeedController right){
-        super(left, right);
-        time.start();
-        time.reset();
-    }
-
-
-@Override
-public void arcadeDrive(double xSpeed, double zRotation) {
-
-    // if(xSpeed > 1/2*maxJerk*time^2-1/2*maxJerk((time-timeWindow)^2)){
-    //     xSpeed = 1/2*maxJerk*time^2-1/2*maxJerk((time-timeWindow)^2);
-    //     // time.
-    // }
+	public SafeDrivetrain(SpeedController left, SpeedController right){
+		super(left, right);
+		time.start();
+		time.reset();
+	}
 
 
-    super.arcadeDrive(xSpeed, zRotation);
-}
-    public void setMaxJerk(double maxJerk) {
-            this.maxJerk = maxJerk;
-        }
+	@Override
+	public void arcadeDrive(double xSpeed, double zRotation) {
+		
+		dt = (time.get() - lastTime);
+		SmartDashboard.putNumber("dt", dt);
+		double limitPower = lastPower; //fix
+		SmartDashboard.putNumber("last Power", lastPower);
 
-    public double getMaxJerk() {
-        return maxJerk;
-    }
+		if (xSpeed != lastPower) {
+			
+			thisAccel = (xSpeed - lastPower) / dt;
+			SmartDashboard.putNumber("thisAccel", thisAccel);
 
-    public void setTimeWindow(){
-        this.timeWindow = timeWindow;
-    }
-    
-    public double getTimeWindow(){
-        return timeWindow;
-    }
+			if (thisAccel > maxAcceleration) {
+				limitPower = lastPower + (maxAcceleration * dt);
+			}
+			if (thisAccel < -maxAcceleration) {
+				limitPower = lastPower - (maxAcceleration * dt);
+			}
+
+			thisAccel = (limitPower - lastPower) / dt;
+			SmartDashboard.putNumber("true accel?", thisAccel);
+
+			lastPower = limitPower;
+			lastTime = time.get();
+		}
+
+		SmartDashboard.putNumber("limit Power", limitPower);
+
+		super.arcadeDrive(limitPower, zRotation);
+	}
 
 
+	public void aarcadeDrive(double xPower, double zRotation) {
 
+		double thisdt = (time.get() - lastTime);
+		if (thisdt > 0.5) {
+			thisdt = 0.5;
+		}
+		SmartDashboard.putNumber("dt", dt);
+
+		SmartDashboard.putNumber("last Power", lastPower);
+		double limitPower = 0;
+
+		thisAccel = (lastPower - lastLastPower) / dt;
+
+		if (thisAccel > maxAcceleration) {
+			SmartDashboard.putBoolean("if", true);
+			limitPower = lastPower + (maxAcceleration * thisdt);
+		}
+		if (thisAccel < -maxAcceleration) {
+			SmartDashboard.putBoolean("if", false);
+			limitPower = lastPower - (maxAcceleration * thisdt);
+		}
+		SmartDashboard.putNumber("(maxAcceleration * thisdt)", (maxAcceleration * thisdt));
+
+		SmartDashboard.putNumber("thisAccel", thisAccel);
+		thisAccel = (limitPower - lastPower) / thisdt;
+		SmartDashboard.putNumber("true accel?", thisAccel);
+
+
+		// // if(xPower > 1/2*maxJerk*time^2-1/2*maxJerk((time-timeWindow)^2)){
+		// //     xPower = 1/2*maxJerk*time^2-1/2*maxJerk((time-timeWindow)^2);
+		// //     // time.
+		// // }
+
+		SmartDashboard.putNumber("limit Power", limitPower);
+		super.arcadeDrive(limitPower, 0);
+
+		if (xPower != lastPower) {
+
+			lastLastPower = lastPower;
+			lastLastTime = lastTime;
+
+			lastPower = limitPower;
+			lastTime = time.get();
+			dt = lastTime - lastLastTime;
+		}
+
+	}
+
+	public void setMaxJerk(double maxJerk) {
+			this.maxJerk = maxJerk;
+		}
+
+	public double getMaxJerk() {
+		return maxJerk;
+	}
+
+	public void setMaxAcceleration(double maxAcceleration) {
+		this.maxAcceleration = maxAcceleration;
+	}
+
+	public double getMaxAcceleration() {
+		return maxAcceleration;
+	}
+
+	public void setTimeWindow(double timeWindow) {
+		this.timeWindow = timeWindow;
+	}
+	
+	public double getTimeWindow() {
+		return timeWindow;
+	}
+
+
+	private double limit(double in, double low, double high) {
+
+		if (in > high) {
+			return high;
+		}
+		if (in < low) {
+			return low;
+		}
+
+		return in;
+	}
 
 
 }
